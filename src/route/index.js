9f33b6e8caf5025e45dcee35c4b7f17271dac05a
Iniva71 <i.kolotilina1971@gmail.com>
2023-05-14 20:03:13 +0300

    snw

--- src/route/index.js
+++ src/route/index.js
@@ -1985,7 +1985,7 @@ router.get('/shopproduct', function (req, res) {
 })
 
 // ================================================================
-outer.get('/shopnews', function (req, res) {
+router.get('/shopnews', function (req, res) {
   // res.render генерує нам HTML сторінку
 
   //            ↙ cюди вводимо назву файлу з сontainer
@@ -2056,7 +2056,7 @@ outer.get('/shopnews', function (req, res) {
           description:
             "NASA's Perseverance rover successfully launches on a mission to explore Mars, with the goal of collecting samples of the planet's surface and searching for signs of ancient microbial life.",
           isTop: false,
-          idNew: true,
+          isNew: true,
         },
         {
           title:
@@ -2064,7 +2064,7 @@ outer.get('/shopnews', function (req, res) {
           description:
             'Shares of GameStop surge as amateur traders on the social media platform Reddit rally to drive up the price, in what some are calling a battle between Wall Street and Main Street.',
           isTop: false,
-          idNew: false,
+          isNew: false,
         },
         {
           title:
@@ -2072,14 +2072,14 @@ outer.get('/shopnews', function (req, res) {
           description:
             'A new study published in the Journal of the American Medical Association suggests that regular exercise can help people live longer, with participants who exercised regularly having a lower risk of premature death.',
           isTop: false,
-          idNew: false,
+          isNew: false,
         },
         {
           title: 'Amazon Expands Grocery Delivery Service',
           description:
             'Amazon announces an expansion of its grocery delivery service, with plans to offer free delivery to Prime members on orders over $35 and to expand its selection of fresh and organic produce.',
           isTop: false,
-          idNew: true,
+          isNew: true,
         },
       ],
     },
@@ -2194,6 +2194,4 @@ outer.get('/shopnews', function (req, res) {
 
 // ================================================================
 
-// ================================================================
 module.exports = router
-z
